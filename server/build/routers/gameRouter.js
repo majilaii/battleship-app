18cdc20c4d0e9dd251e74dd02f014c91d8c6b849
Kuong Ao Ieong <majilaiwife@gmail.com>
2023-03-28 23:27:25 +0100

    changed two endpoints to a get instead of post

--- server/build/routers/gameRouter.js
+++ server/build/routers/gameRouter.js
@@ -11,6 +11,6 @@ exports.gameRouter = gameRouter;
 gameRouter.post("/singleplayer/game", gameController_1.startGame); // User presses start game and begins the game against a bot
 gameRouter.post("/singleplayer/gamerandom", gameController_1.startGameRandom); // User randomizes his ship placements against a bot and starts game
 gameRouter.post("/singleplayer/shoot", gameController_1.shoot); // User/Bot shoots the opponent's board
-gameRouter.post("/singleplayer/getboard", gameController_1.getBoard); //This is not needed for the frontend but can be useful for pure API usage
-gameRouter.post("/singleplayer/getcomputerboard", gameController_1.getComputerBoard); //This is not needed due to it being cheating but can be useful for pure API usage
+gameRouter.get("/singleplayer/getboard", gameController_1.getBoard); //This is not needed for the frontend but can be useful for pure API usage
+gameRouter.get("/singleplayer/getcomputerboard", gameController_1.getComputerBoard); //This is not needed due to it being cheating but can be useful for pure API usage
 gameRouter.post("/singleplayer/reset", gameController_1.reset); // Reset Board
--- server/routers/gameRouter.ts
+++ server/routers/gameRouter.ts
@@ -12,8 +12,8 @@ const gameRouter = Express.Router();
 gameRouter.post("/singleplayer/game", startGame); // User presses start game and begins the game against a bot
 gameRouter.post("/singleplayer/gamerandom", startGameRandom); // User randomizes his ship placements against a bot and starts game
 gameRouter.post("/singleplayer/shoot", shoot); // User/Bot shoots the opponent's board
-gameRouter.post("/singleplayer/getboard", getBoard); //This is not needed for the frontend but can be useful for pure API usage
-gameRouter.post("/singleplayer/getcomputerboard", getComputerBoard); //This is not needed due to it being cheating but can be useful for pure API usage
+gameRouter.get("/singleplayer/getboard", getBoard); //This is not needed for the frontend but can be useful for pure API usage
+gameRouter.get("/singleplayer/getcomputerboard", getComputerBoard); //This is not needed due to it being cheating but can be useful for pure API usage
 gameRouter.post("/singleplayer/reset", reset); // Reset Board
 
 export { gameRouter };
